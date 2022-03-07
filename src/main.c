b6ffdf9011a8e768c708debd13f8cb0e1f2b97aa
Jan-Loettgen <janlucaloettgen@gmail.com>
2022-03-07 09:46:49 +0000

    fixed bug in tranpose now working

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 
 int main(){
 
-    tensor_2d* mat1 = mat_rand(16, 5);
+    tensor_2d* mat1 = mat_rand(2, 5);
     tensor_2d* mat2 = mat_rand(5, 3);
 
     //NOTE SEGMENTATION FAULT WHEN row mat A > col MAT B
@@ -14,11 +14,10 @@ int main(){
     tensor_2d* mat_c = mat_mul(mat1, mat2);
     tensor_2d* mat_T = mat_transpose(mat1);
 
-
-    // mat_print(mat1);
-    // mat_print(mat2);
-    // mat_print(mat_c);
-    // mat_print(mat_T);
+    mat_print(mat1);
+    mat_print(mat2);
+    mat_print(mat_c);
+    mat_print(mat_T);
 
     mat_free(&mat1);
     mat_free(&mat2);
