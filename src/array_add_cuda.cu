704bf710b7b841a28b598d90d14feaf7aad59f75
Jan-Loettgen <janlucaloettgen@gmail.com>
2022-03-16 09:33:46 +0000

    cuda matrix addition

--- src/array_add_cuda.cu
+++ src/array_add_cuda.cu
@@ -1,13 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include <cuda.h>
+
+#include "tensor_2d.h"
 
 #define NUM_THREADS_PER_BLOCK 256.0
 
 
 __global__
-void vecAddKernal(float *A, float *B, float *C, int n){
+void _vecAddKernal(float *A, float *B, float *C, int n){
     //global thread id used for indexing.
     int i = blockDim.x*blockIdx.x + threadIdx.x;
 
@@ -18,92 +19,76 @@ void vecAddKernal(float *A, float *B, float *C, int n){
 }
 
 __host__
-int array_add_C(float *h_A, float *h_B, float *h_C, int num_elems, int size){
-    //allocate device memory for arrays A, B, and C.
-    float *d_A, *d_B, *d_C;
-    cudaError_t err1 = cudaMalloc((void**) &d_A, size);
+int mat_add_C(tensor_2d* mat_a, tensor_2d* mat_b, tensor_2d* mat_out){
+    if (mat_a == NULL || mat_b == NULL|| mat_out == NULL){
+        return 1;
+    }
+    else if (mat_a->n_cols != mat_b->n_cols || mat_a->n_cols != mat_out->n_cols){
+        return 2;
+    }
+    else if (mat_a->n_rows != mat_b->n_rows || mat_a->n_rows != mat_out->n_rows){
+        return 2;
+    }
+
+    unsigned int size = mat_a->n_elems*sizeof(double);
+
+    //allocate device memory for arrays A, B, and output array.
+    float *d_data_A, *d_data_B, *d_data_out;
+    cudaError_t err1 = cudaMalloc((void**) &d_data_A, size);
     if (err1 != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err1),__FILE__,__LINE__);
-        return 11;
+        return 101;
     }
 
-    cudaError_t err2 = cudaMalloc((void**) &d_B, size);
+    cudaError_t err2 = cudaMalloc((void**) &d_data_B, size);
     if (err2 != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err2),__FILE__,__LINE__);
-        return 11;
+        return 101;
     }
 
-    cudaError_t err3 = cudaMalloc((void**) &d_C, size);
+    cudaError_t err3 = cudaMalloc((void**) &d_data_out, size);
     if (err3 != cudaSuccess) {
         printf("%s in %s at line %d\n", cudaGetErrorString(err3),__FILE__,__LINE__);
-        return 11;
+        return 101;
     }
 
     //copy contents of bariables h_A, and h_B to d_A, and d_B
-    cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data_A, mat_a->data, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_data_B, mat_b->data, size, cudaMemcpyHostToDevice);
 
     //kernal invocation
-    vecAddKernal<<<ceil(num_elems/NUM_THREADS_PER_BLOCK), NUM_THREADS_PER_BLOCK>>>(d_A, d_B, d_C, num_elems);
+    _vecAddKernal<<<ceil(mat_a->n_elems/NUM_THREADS_PER_BLOCK), NUM_THREADS_PER_BLOCK>>>(d_data_A, d_data_B, d_data_out, mat_a->n_elems);
 
     //copy device memory to host memory
-    cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(mat_out->data, d_data_out, size, cudaMemcpyDeviceToHost);
 
     //free device memory
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+    cudaFree(d_data_A);
+    cudaFree(d_data_B);
+    cudaFree(d_data_out);
 
     return 0;
-
 }
 
 
 int main(){
-    int num_elems = 24;
-    unsigned int size = num_elems*sizeof(float);
 
-    //allocate host memory for arrays A, B, and C.
-    float *h_A, *h_B, *h_C;
-    h_A = (float*)malloc(size);
-    h_B = (float*)malloc(size);
-    h_C = (float*)malloc(size);
+    tensor_2d* mat_a = mat_make(24, 24);
+    tensor_2d* mat_b = mat_make(24, 24);
+    tensor_2d* mat_c = mat_make(24, 24);
 
-    ///set arrays to contain random values.
-    for (int i = 0; i< 10; i++){
-        h_A[i] = rand()/2147483647.0;
-    }
+    mat_rand(0.0, 1.0, mat_a);
+    mat_zeros(mat_b);
 
-    for (int i = 0; i< 10; i++){
-        h_B[i] = rand()/2147483647.0;
-    }
+    mat_add_C(mat_a, mat_b, mat_c);
 
-    for (int i = 0; i< 10; i++){
-        h_C[i] = rand()/2147483647.0;
-    }
-
-
-    array_add_C(h_A, h_B, h_C, num_elems, size);
-
-
-    //do stuff with final array
-    for (int i = 0; i< 10; i++){
-        printf("%f ", h_A[i]);
-    }
-    printf("\n");
-    for (int i = 0; i< 10; i++){
-        printf("%f ", h_B[i]);
-    }
-    printf("\n");
-    for (int i = 0; i< 10; i++){
-        printf("%f ", h_C[i]);
-    }
-    printf("\n");
+    mat_print(mat_a);
+    mat_print(mat_b);
+    mat_print(mat_c);
 
     //free host memory
-    free(h_A);
-    free(h_B);
-    free(h_C);
-
+    mat_free(&mat_a);
+    mat_free(&mat_b);
+    mat_free(&mat_c);
     return 0;
 }
